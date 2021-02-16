da8904737e04b7c0faa989605a37f6cf5fd2b10f
realjasomo <jason.cramer6@gmail.com>
2021-02-16 00:52:33 -0500

    finished first iteration of portfolio

--- src/components/pages/AboutPage/index.tsx
+++ src/components/pages/AboutPage/index.tsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
+import desertme from '../../../res/desertme.jpg'
+import styles from './about.module.css'
+
 export default class AboutPage extends Component {
     render() {
         return (
-            <div>
-                About
+            <div style={{ marginLeft:'1rem', height:'100vh', overflow:'auto'}}>
+                <h1 style={{flexBasis:'100%'}}>About me</h1>
+                <img className={styles.picture} src={desertme} alt="Crazy developer in a desert"/>
+                <p className={styles.text}>I am Jason Cramer, a 20yr old developer studying at Rose-Hulman Institute of technology in Computer Science and Software Engineering. 
+                   I am currently sophomore standing and looking for internships focusing in web application development, cybersecurity, artifical intelligence, or machine learning. 
+                   My favorite programming language would have to be Javascript, although the type safety of Typescript has been catching my eye lately. 
+                   If you would like to get to know me further, feel free to reach out by email via <a style={{textDecoration: "none", color: "#444"}} href="mailto:cramerj1@rose-hulman.edu">cramerj1@rose-hulman.edu</a></p>
+                <div className={styles.bottomFlurish}></div>
             </div>
         )
     }
--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import ReactTypingEffect from 'react-typing-effect'
+import { IconButton } from '@material-ui/core'
+
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import me from '../../../res/me.png'
 import styles from './home.module.css'
 
@@ -10,9 +14,20 @@ export default class HomePage extends Component {
                 <img className={styles.image} src={me} alt="Jason cramer"/>
                 <h1>Hi, I'm <strong>Jason Cramer</strong></h1>
                <h2>I am a <ReactTypingEffect 
-                    text={["Full-Stack developer", "Software Engineer"]}
+                    text={["Student","Full-Stack developer", "Software Engineer"]}
                     speed={100}
+                    eraseSpeed={100}
+                    eraseDelay={2000}
+                    typingDelay={1000}
                 /></h2>
+                <div className={styles.link}>
+                    <IconButton href="https://linkedin.com/in/jason-cramer-617881190">
+                        <LinkedInIcon />
+                    </IconButton>
+                    <IconButton href="https://github.com/RealJasomo">
+                        <GitHubIcon/>
+                    </IconButton>
+                </div>
             </div>
         )
     }

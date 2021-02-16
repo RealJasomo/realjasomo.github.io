b6ff6ad261ff9a567f78e4711466b098ca13d536
realjasomo <jason.cramer6@gmail.com>
2021-02-15 23:57:23 -0500

    Made basic homepage

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import ReactTypingEffect from 'react-typing-effect'
+import me from '../../../res/me.png'
+import styles from './home.module.css'
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
-                Home
+            <div className={styles.flex}>
+                <img className={styles.image} src={me} alt="Jason cramer"/>
+                <h1>Hi, I'm <strong>Jason Cramer</strong></h1>
+               <h2>I am a <ReactTypingEffect 
+                    text={["Full-Stack developer", "Software Engineer"]}
+                    speed={100}
+                /></h2>
             </div>
         )
     }
